f787a830852d4e5660793dbd9b7d083557661eef
Raffaele Sollecito <raffaelesollecito@MacBook-Pro-di-FRANCESCO.local>
2019-03-19 00:48:58 +0100

    repo init

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatNativeDateModule, MatDatepickerModule, MatIconModule, MatButtonModule, MatCheckboxModule,
+  MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatRadioModule, 
+  MatListModule 
+} from  '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatNativeDateModule, MatDatepickerModule, MatIconModule, MatButtonModule, MatCheckboxModule,
+    MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatRadioModule, 
+    MatListModule
+  ],
+  exports: [
+    MatNativeDateModule, MatDatepickerModule, MatIconModule, MatButtonModule, MatCheckboxModule,
+    MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatRadioModule, 
+    MatListModule
+  ]
+})
+export class MaterialModule { }
